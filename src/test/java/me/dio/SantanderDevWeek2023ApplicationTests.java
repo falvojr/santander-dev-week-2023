3ea353ad1a8153862bdf8dff144be494e8fe0180
Venilton FalvoJr <falvojr@gmail.com>
2023-07-21 06:27:06 -0300

    Initial commit

---
+++ src/test/java/me/dio/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
